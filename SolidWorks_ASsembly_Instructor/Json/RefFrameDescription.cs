21732b43d32dbec5ba082a1713113fd38fee9d22
Terei <terei@match.uni-hannover.de>
2024-02-15 13:48:05 +0100

    Pieg Demonstrator

--- SolidWorks_ASsembly_Instructor/Json/RefFrameDescription.cs
+++ SolidWorks_ASsembly_Instructor/Json/RefFrameDescription.cs
@@ -13,18 +13,21 @@ namespace SolidWorks_ASsembly_Instructor
             public string name;
             public string type;
             public CoordinateSystemDescription transformation;
+            public RefFrameConstraints constraints;
 
             public RefFrameDescription()
             {
                 name = string.Empty;
                 type = string.Empty;
                 transformation = new CoordinateSystemDescription();
+                constraints = new RefFrameConstraints();
             }
             public RefFrameDescription(string _type)
             {
                 name = string.Empty;
                 type = _type;
                 transformation = new CoordinateSystemDescription();
+                constraints = new RefFrameConstraints();
             }
     }
 }
